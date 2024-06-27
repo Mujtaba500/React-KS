502b5d4af9a6c83199c3061cda145c8d7fc9f101
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-06-27 23:04:17 +0500

    Search Products functionality

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,16 +4,32 @@ import { useEffect, useState } from "react";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [searchedProducts, setSearchedProducts] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
   const getProducts = async () => {
     const response = await axiosInstance.get("/products");
     setProducts(response.data);
+    setSearchedProducts(response.data);
   };
 
   useEffect(() => {
     getProducts();
   }, []);
 
-  const renderProducts = products.map((product) => {
+  useEffect(() => {
+    // apply filteration
+
+    const searchResults = products.filter((product) => {
+      if (product.title.toLowerCase().includes(searchString.toLowerCase()))
+        return true;
+      else return false;
+    });
+
+    setSearchedProducts(searchResults);
+  }, [searchString]);
+
+  const renderProducts = searchedProducts.map((product) => {
     return (
       <ProductCard
         title={product.title}
@@ -29,15 +45,15 @@ export default function Products() {
 
   return (
     <>
-      <h1>Products</h1>
-      {/* <input
+      <input
         type="text"
         placeholder="Search"
         value={searchString}
         onChange={(e) => {
           setSearchString(e.target.value);
         }}
-      /> */}
+        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+      />
       <div className="grid w-full grid-cols-3 gap-3">{renderProducts}</div>
     </>
   );
