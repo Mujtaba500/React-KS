6edb31ba413b159e4cedaee0aaaf44001cf95dc2
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2024-06-28 11:31:08 +0500

    Form validation using Yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
   const formik = useFormik({
@@ -6,57 +7,78 @@ const Login = () => {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Password must be 8 characters long")
+        .matches(/[0-9]/, "Password requires a number")
+        .matches(/[a-z]/, "Password requires a lowercase letter")
+        .matches(/[A-Z]/, "Password requires an uppercase letter")
+        .matches(/[^\w]/, "Password requires a symbol")
+        .required("Required"),
+    }),
     onSubmit: (values) => {
+      console.log(values);
       alert(JSON.stringify(values, null, 2));
     },
   });
 
   return (
-    <div class="w-full max-w-xs m-auto">
-      <h1 class="text-center text-gray-700 text-sm font-bold">Log In</h1>
+    <div className="w-full max-w-xs m-auto">
+      <h1 className="text-center text-gray-700 text-sm font-bold">Log In</h1>
       <form
         onSubmit={formik.handleSubmit}
-        class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
       >
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             type="email"
             placeholder="Email"
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div>{formik.errors.email}</div>
+          ) : null}
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
           >
             Password
           </label>
           <input
-            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
             onChange={formik.handleChange}
             value={formik.values.password}
           />
-          <p class="text-red-500 text-xs italic">Please choose a password.</p>
+          {formik.touched.password && formik.errors.password ? (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.password}
+            </p>
+          ) : null}
         </div>
-        <div class="flex items-center justify-between">
+        <div className="flex items-center justify-between">
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
           >
             Sign In
           </button>
           <a
-            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
             href="#"
           >
             Forgot Password?
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const SignUp = () => {
   const formik = useFormik({
@@ -8,95 +9,121 @@ const SignUp = () => {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("Required"),
+      lastName: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Password must be 8 characters long")
+        .matches(/[0-9]/, "Password requires a number")
+        .matches(/[a-z]/, "Password requires a lowercase letter")
+        .matches(/[A-Z]/, "Password requires an uppercase letter")
+        .matches(/[^\w]/, "Password requires a symbol")
+        .required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
 
   return (
-    <div class="w-full max-w-xs m-auto">
-      <h1 class="text-center text-gray-700 text-sm font-bold">Sign Up</h1>
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
+    <div className="w-full max-w-xs m-auto">
+      <h1 className="text-center text-gray-700 text-sm font-bold">Sign Up</h1>
+      <form
+        onSubmit={formik.handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="firstname"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="firstName"
           >
             First Name
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="firstname"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="firstName"
             type="text"
             placeholder="FirstName"
             onChange={formik.handleChange}
             value={formik.values.firstName}
           />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.firstName}
+            </p>
+          ) : null}
         </div>
-        <div class="mb-4">
+        <div className="mb-4">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="lastname"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="lastName"
           >
             Last Name
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="lastname"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="lastName"
             type="text"
             placeholder="LastName"
             onChange={formik.handleChange}
             value={formik.values.lastName}
           />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.lastName}
+            </p>
+          ) : null}
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             type="email"
             placeholder="Email"
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <p className="text-red-500 text-xs italic">{formik.errors.email}</p>
+          ) : null}
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            class="block text-gray-700 text-sm font-bold mb-2"
-            for="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
           >
             Password
           </label>
           <input
-            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             type="password"
             placeholder="******************"
             onChange={formik.handleChange}
             value={formik.values.password}
           />
-          <p class="text-red-500 text-xs italic">Please choose a password.</p>
+          {formik.touched.password && formik.errors.password ? (
+            <p className="text-red-500 text-xs italic">
+              {formik.errors.password}
+            </p>
+          ) : null}
         </div>
-        <div class="flex items-center justify-between">
+        <div className="flex items-center justify-between">
           <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
           >
-            Sign In
+            Sign Up
           </button>
-          <a
-            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
       </form>
-      <p class="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
-      </p>
     </div>
   );
 };
